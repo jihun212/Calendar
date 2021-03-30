427fa029b4cb515d996b3da135486e1201428c3e
jihun212 <57865537+jihun212@users.noreply.github.com>
2021-03-30 18:34:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/calendar/MonthViewActivity.java
@@ -0,0 +1,51 @@
+package com.example.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MonthViewActivity extends AppCompatActivity {
+    int year;
+    int month;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView yearMonthTV = findViewById(R.id.year_month);
+
+        //Calendar cal = Calendar.getInstance();
+        //cal.set(2021,3,1);
+        //int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+
+
+        Intent intent = getIntent();
+        year = intent.getIntExtra("year",-1);
+        month = intent.getIntExtra("month",-1);
+
+        if(year ==-1 || month == -1){
+            year = Calendar.getInstance().get(Calendar.YEAR);
+            month = Calendar.getInstance().get(Calendar.MONTH);
+        }
+
+        yearMonthTV.setText( year + "년"+ (month+1) + "월");
+
+        Button prevBtn = findViewById(R.id.previous);
+        prevBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MonthViewActivity.class);
+                intent.putExtra("year",year);
+                intent.putExtra("month",month -1);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
