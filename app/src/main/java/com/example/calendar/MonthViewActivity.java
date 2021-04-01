be18b744e52bbeb742d9ab59024688319065e604
LimJunBeom <ljb960514@naver.com>
2021-04-01 10:17:19 +0900

    edit

--- app/src/main/java/com/example/calendar/MonthViewActivity.java
+++ app/src/main/java/com/example/calendar/MonthViewActivity.java
@@ -10,15 +10,59 @@ import android.widget.TextView;
 
 import java.util.Calendar;
 
-public class MonthViewActivity extends AppCompatActivity {
+public class MonthViewActivity extends AppCompatActivity implements View.OnClickListener {
 
-    int year;
-    int month;
+    private Button previous;
+    private Button next;
+    private Calendar calendar;
+
+    private int year, month;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH) + 1;
+
+        previous = findViewById(R.id.previous);
+        previous.setOnClickListener(this);
+
+        next = findViewById(R.id.next);
+        next.setOnClickListener(this);
+    }
+    private void setGridCellAdapterToDate(int month, int year) {
+
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == previous) {
+            if (month <= 1) {
+                month = 12;
+                year--;
+            } else {
+                month--;
+            }
+            setGridCellAdapterToDate(month, year);
+        }
+        if (v == next) {
+            if (month > 11) {
+                month = 1;
+                year++;
+            } else {
+                month++;
+            }
+            setGridCellAdapterToDate(month, year);
+        }
+    }
+
+
+
+
+
 
         Intent intent = getIntent();
         year = intent.getIntExtra("year",-1);
@@ -29,17 +73,18 @@ public class MonthViewActivity extends AppCompatActivity {
             month = Calendar.getInstance().get(Calendar.MONTH);
         }
 
-        //Calendar cal = Calendar.getInstance();
-        //cal.set(2021,3,1);
-        //int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        Calendar cal = Calendar.getInstance();
+        cal.set(2021,3,1);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
 
 
         TextView yearMonthTV = findViewById(R.id.year_month); // year_month 가져오기
         yearMonthTV.setText(year + "년" + month + "월");
 
 
-        Button prevBtn = findViewById(R.id.previous);
-        prevBtn.setOnClickListener(new View.OnClickListener(){
+        previous = findViewById(R.id.previous);
+        previous.setOnClickListener(new View.OnClickListener()
+        {
 
             @Override
             public void onClick(View v) {
@@ -53,8 +98,8 @@ public class MonthViewActivity extends AppCompatActivity {
         });
 
 
-        Button nextBtn = findViewById(R.id.next);
-        nextBtn.setOnClickListener(new View.OnClickListener(){
+        next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
@@ -65,6 +110,10 @@ public class MonthViewActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        });
+        }
+
+
+
+    });
     }
 }
