6df384c23424bb98cb5e5452ef442c9585c0b864
jihun212 <1871280@hansung.ac.kr>
2021-05-02 03:59:34 +0900

    액션바추가

--- app/src/main/java/com/example/calendar/MonthViewActivity.java
+++ app/src/main/java/com/example/calendar/MonthViewActivity.java
@@ -1,6 +1,9 @@
 package com.example.calendar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -9,6 +12,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MonthViewActivity extends AppCompatActivity {
 
@@ -18,6 +23,37 @@ public class MonthViewActivity extends AppCompatActivity {
     int curYear;    //현재년도
     int curMonth;   //현재달
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.month_view:
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_container, new MonthViewFragment());
+                fragmentTransaction.commit();
+                Toast.makeText(getApplicationContext(), "month_view", Toast.LENGTH_SHORT).show();
+                return true;
+
+
+            case R.id.week_view:
+
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_container, new WeekViewFragment());
+                fragmentTransaction.commit();
+                Toast.makeText(getApplicationContext(), "week_view", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
