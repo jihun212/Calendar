2b7a6de177bdf6a30899f4792ca184d26daebf1f
jihun212 <1871280@hansung.ac.kr>
2021-06-12 15:25:02 +0900

    저장, 취소 버튼

--- app/src/main/java/com/example/calendar/ScheduleActivity.java
+++ app/src/main/java/com/example/calendar/ScheduleActivity.java
@@ -1,15 +1,94 @@
 package com.example.calendar;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.nio.Buffer;
 
 
 public class ScheduleActivity extends AppCompatActivity {
 
+    EditText schedule_title;
+    TimePicker schedule_start;
+    TimePicker schedule_end;
+    EditText place;
+    EditText memo;
+
+    private DBHelper mDbHelper;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule);
+
+        schedule_title=findViewById(R.id.schedule_title);
+
+        schedule_start=findViewById(R.id.schedule_start);
+        int start_hour= TimePickerSplit(schedule_start)[0];
+        int start_min= TimePickerSplit(schedule_start)[1];
+
+        schedule_end=findViewById(R.id.schedule_end);
+        int end_hour= TimePickerSplit(schedule_end)[0];
+        int end_min= TimePickerSplit(schedule_end)[1];
+
+        place=findViewById(R.id.place);
+        memo=findViewById(R.id.memo);
+
+        mDbHelper = new DBHelper(this);
+
+
+        Button save_btn = (Button)findViewById(R.id.save);
+        Button cancel_btn = (Button)findViewById(R.id.cancel);
+        Button delete_btn = (Button)findViewById(R.id.delete);
+
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ScheduleActivity.this,"저장", Toast.LENGTH_SHORT).show();
+                mDbHelper.insertUserBySQL(
+                        schedule_title.getText().toString(),
+                        start_hour,
+                        start_min,
+                        end_hour,
+                        end_min,
+                        place.getText().toString(),
+                        memo.getText().toString()
+                );
+            }
+        });
+
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        delete_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    int [] TimePickerSplit(TimePicker timepicker){
+        int hour, min;
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){
+            hour = timepicker.getHour();
+            min= timepicker.getMinute();
+        }else{
+            hour = timepicker.getCurrentHour();
+            min=timepicker.getCurrentMinute();
+        }
+        return new int[] {hour,min};
     }
 
 }
